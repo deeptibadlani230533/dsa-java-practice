62631019b58e1cdec9c52924825d22ef41eba6dd
Deepti Badlani <deeptibadlani230533@acropolis.in>
2026-04-19 19:42:16 +0530

    Added hash solutions

--- Arrays/MaxDiffOptimized.java
+++ Arrays/MaxDiffOptimized.java
@@ -4,8 +4,8 @@ class Solution {
     public int maximumDifference(int[] nums) {
 
         int n=nums.length;
-        int minSoFar=nums[0];
-        int maxDiff=-1;         //i
+        int minSoFar=nums[0];       //i
+        int maxDiff=-1;         
 
         for(int j=1;j<n;j++)
         {
---
+++ Arrays/TrappingRainWaterBrute.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int trap(int[] height) {
+        int res=0;
+        int n=height.length;
+        for(int i=1;i<n-1;i++)
+        {
+            int lmax=height[i];
+            for(int j=0;j<i;j++)
+            {
+                lmax=Math.max(lmax,height[j]);
+            }
+
+            int rmax=height[i];
+            for(int j=i+1;j<n;j++)
+            {
+                rmax=Math.max(rmax,height[j]);
+            }
+
+            res=res+(Math.min(lmax,rmax)-height[i]);
+        }
+        return res;
+    }
+}
