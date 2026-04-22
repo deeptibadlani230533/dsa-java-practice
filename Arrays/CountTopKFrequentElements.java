7043c70485d084a0273a02c85c3d9a3031533a99
Deepti Badlani <deeptibadlani230533@acropolis.in>
2026-04-22 06:30:23 +0530

    Added Top K Frequent Elements solution using HashMap and Bucket Sort

---
+++ Arrays/CountTopKFrequentElements.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        Map<Integer,Integer> map =new HashMap<>();
+        
+        for(int i=0;i<nums.length;i++)
+        {
+            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+        }
+        List <Integer> key=new ArrayList<>(map.keySet());
+        Collections.sort(key,(a,b)-> map.get(b) - map.get(a));
+
+        int[] result = new int[k];
+        for(int i = 0; i < k; i++)
+        {
+            result[i] = key.get(i);
+        }
+        return result;
+
+        
+    }
+}
